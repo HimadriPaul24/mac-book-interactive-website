c1e06161749e5c5d60f8172984164471de9e8da3
HimadriPaul24 <himadripaul821@gmail.com>
2021-08-21 13:16:47 +0600

    second message

--- js/index.js
+++ js/index.js
@@ -1,1 +1,4 @@
-console.log('i am hima');
+document.getElementById('memory-8GB').addEventListener('click',function (){
+    // console.log('8 GB clicked');
+    const caseInput = document.getElementById
+})
